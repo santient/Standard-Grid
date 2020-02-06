b31d3623f3ac7018285d45701c7966d769bb49e2
Amir Zadeh <abagherz@cs.cmu.edu>
2020-02-05 19:25:38 -0500

    2/5/2020

--- example/ml_root/results/aa63f65af9fa3a606abc7a1274343a6d9f683c79863218ef1e2820b67a6c81914500924e4974a70f43d069621006a42f5dcbf84241e6c30b1c0af965ca6537ba/instances/46ce7143c8ce2c0d04baa055eb06eee244571b2a8db7f9a2c9ba8937acb9c6e575583525ddfd3294c04771adb3e85d66d645584fe2a15aae06264b7eddad0f03/46ce7143c8ce2c0d04baa055eb06eee244571b2a8db7f9a2c9ba8937acb9c6e575583525ddfd3294c04771adb3e85d66d645584fe2a15aae06264b7eddad0f03.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-run_grid_instance (){
-	python  ../../../../ml_code_json.py  --bs 64 --lr_net 0.0001 --epochs 100000000000 
- 	echo $? > STANDARDGRID_instance_output
-}
-run_grid_instance
