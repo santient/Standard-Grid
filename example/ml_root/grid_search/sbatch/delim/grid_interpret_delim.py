fdbf74a851f1107e3bbb0d58c7570def27454296
Amir Zadeh <abagherz@cs.cmu.edu>
2020-02-05 19:25:25 -0500

    2/5/2020

--- example/ml_root/grid_search/sbatch/delim/grid_interpret_delim.py
+++
@@ -1,9 +0,0 @@
-import standard_grid
-import pickle
-import os 
-import sys
-
-if __name__=="__main__":
-	grid=pickle.load(open(sys.argv[1],"rb"))
-	grid.delim_interpret("results/best.txt","interpretation.csv")
-
--- example/ml_root/grid_search/sbatch/delim/grid_status.py
+++
@@ -1,8 +0,0 @@
-import standard_grid
-import pickle
-import os 
-import sys
-
-if __name__=="__main__":
-	grid=pickle.load(open(sys.argv[1],"rb"))
-	grid.get_status()
